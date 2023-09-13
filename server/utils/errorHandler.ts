e268486d44ef3b8e0c62c067c115419889c996e1
Umar Ahmed <umarahmedse@outlook.com>
2023-09-13 22:28:42 +0500

    fixed typecasting bug

--- server/utils/errorHandler.ts
+++ server/utils/errorHandler.ts
@@ -57,12 +57,12 @@ const sendErrorProd = (err: AppError, req: Request, res: Response) => {
 
 export default (err: any, req: Request, res: Response, next: NextFunction) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+  err.status = err.status || "Internal Server Error";
 
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === "production") {
-    let error: AppError = { ...err };
+    let error = { ...err };
     error.name = err.name;
     error.message = err.message;
     if (error.name === "CastError") error = handleCastErrorDB(error);
