c44a7a7b6157cc04cbc4ba6a733bccd38c729a8a
Umar Ahmed <umarahmedse@outlook.com>
2023-09-14 07:39:42 +0500

    created mail handler

---
+++ server/utils/sendMail.ts
@@ -0,0 +1,37 @@
+import nodemailer, { Transporter } from "nodemailer";
+import ejs from "ejs";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+interface Emailoptions {
+  email: string;
+  subject: string;
+  template: string;
+  data: { [key: string]: any };
+}
+
+const sendMail = async (options: Emailoptions): Promise<void> => {
+  const transporter: Transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT || "587"),
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+  const { email, subject, template, data } = options;
+  //get the mail template file
+  const templatePath = path.join(__dirname, "../mails", template);
+
+  //   render the email template with ejs
+  const html = await ejs.renderFile(templatePath, data);
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: email,
+    subject,
+    html,
+  };
+  await transporter.sendMail(mailOptions);
+};
+export default sendMail;
