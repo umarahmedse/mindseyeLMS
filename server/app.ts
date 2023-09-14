c02cd46008623c44340f1a5d747b90f99534b58b
Umar Ahmed <umarahmedse@outlook.com>
2023-09-14 12:49:57 +0500

    added routes,morgan and imports

--- server/app.ts
+++ server/app.ts
@@ -2,9 +2,14 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import errorHandler from "./utils/errorHandler";
+import userRoutes from "./routes/user.routes";
 dotenv.config();
 const app = express();
+//logger
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+else app.use(morgan("common"));
 //body parser
 app.use(express.json({ limit: "50mb" }));
 //cookie parser
@@ -15,8 +20,8 @@ app.use(
     origin: process.env.ORIGINS,
   })
 );
+app.use("/api/v1", userRoutes);
 //testing api
-
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
     success: true,
