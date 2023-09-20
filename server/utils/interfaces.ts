bdb188aa54a8d735b1cab5298ce18d34f5635082
Umar Ahmed <umarahmedse@outlook.com>
2023-09-20 10:59:56 +0500

    added login user interface

--- server/utils/interfaces.ts
+++ server/utils/interfaces.ts
@@ -12,3 +12,14 @@ export interface InterfaceActivationRequest {
   activation_token: string;
   activation_code: string;
 }
+export interface InterfaceLoginUser {
+  email: string;
+  password: string;
+}
+export interface InterfaceTokenOptions {
+  expires: Date;
+  maxAge: number;
+  httpOnly: boolean;
+  sameSite: "lax" | "strict" | "none" | undefined;
+  secure?: boolean;
+}
