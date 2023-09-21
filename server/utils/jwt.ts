c437a7df7fcadaf92926051d1ada766bb040a8b3
Umar Ahmed <umarahmedse@outlook.com>
2023-09-21 11:16:25 +0500

    minor adjustments

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -3,7 +3,27 @@ import { Response } from "express";
 import { InterfaceUser } from "../models/user-model";
 import redis from "./redis";
 import { InterfaceTokenOptions } from "./interfaces";
+export const accessTokenExpires = parseInt(
+  process.env.ACCESS_TOKEN_EXPIRES || "300",
+  10
+);
+export const refreshTokenExpires = parseInt(
+  process.env.REFRESH_TOKEN_EXPIRES || "1200",
+  10
+);
+export const accessTokenOptions: InterfaceTokenOptions = {
+  expires: new Date(Date.now() + accessTokenExpires * 60 * 1000),
+  maxAge: accessTokenExpires * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
 
+export const refreshTokenOptions: InterfaceTokenOptions = {
+  expires: new Date(Date.now() + refreshTokenExpires * 60 * 1000),
+  maxAge: refreshTokenExpires * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
 export const sendToken = (
   user: InterfaceUser,
   statusCode: number,
@@ -14,27 +34,7 @@ export const sendToken = (
   //upload session to redis
   redis.set(user._id, JSON.stringify(user) as any);
   //parse env variables to integrate with fallback values
-  const accessTokenExpires = parseInt(
-    process.env.ACCESS_TOKEN_EXPIRES || "300",
-    10
-  );
-  const refreshTokenExpires = parseInt(
-    process.env.REFRESH_TOKEN_EXPIRES || "1200",
-    10
-  );
-  const accessTokenOptions: InterfaceTokenOptions = {
-    expires: new Date(Date.now() + accessTokenExpires * 1000),
-    maxAge: accessTokenExpires * 60 * 1000,
-    httpOnly: true,
-    sameSite: "lax",
-  };
 
-  const refreshTokenOptions: InterfaceTokenOptions = {
-    expires: new Date(Date.now() + refreshTokenExpires * 1000),
-    maxAge: refreshTokenExpires * 60 * 1000,
-    httpOnly: true,
-    sameSite: "lax",
-  };
   if ((process.env.NODE_ENV = "production")) {
     accessTokenOptions.secure = true;
   }
