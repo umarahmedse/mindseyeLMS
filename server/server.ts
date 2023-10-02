54631a6bc27f11446234bb581d0c956901ec082e
Umar Ahmed <umarahmedse@outlook.com>
2023-10-02 08:19:53 +0500

    configured Cloudinary for uploads

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,12 @@
 import app from "./app";
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 import connectDB from "./utils/db";
 //create server
 app.listen(process.env.PORT, () => {
