370cee8b1f7fc71c605c3b1b64711d643fddf3eb
Umar Ahmed <umarahmedse@outlook.com>
2023-10-04 08:27:34 +0500

    added new methods and routes for course get

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -4,6 +4,7 @@ import AppError from "../utils/AppError";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
 import courseModel from "../models/course-model";
+import redis from "../utils/redis";
 //CREATE COURSE
 export const uploadCourse = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -21,7 +22,7 @@ export const uploadCourse = catchAsync(
     createCourse(data, res, next);
   }
 );
-//EDIT COURSE  4:57
+//EDIT COURSE
 export const editCourse = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const data = req.body;
@@ -49,3 +50,85 @@ export const editCourse = catchAsync(
     });
   }
 );
+//GET SINGLE COURSE (UNPURCHASED)
+
+export const getSingleCourse = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const courseId = req.params.id;
+    if (!courseId) {
+      return next(new AppError("course id is required", 400));
+    }
+    const isCacheExisits = await redis.get(courseId);
+    if (isCacheExisits) {
+      const course = JSON.parse(isCacheExisits);
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } else {
+      const course = await courseModel
+        .findById(courseId)
+        .select(
+          "-courseData.videoUrl -courseData.suggeston -courseData.questions -courseData.links"
+        );
+
+      if (!course) {
+        return next(new AppError("course not found", 400));
+      }
+      await redis.set(courseId, JSON.stringify(course));
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    }
+  }
+);
+//GET ALL COURSES (UNPURCHASED)
+
+export const getAllCourses = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const isCacheExisits = await redis.get("allCourses");
+    if (isCacheExisits) {
+      const course = JSON.parse(isCacheExisits);
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } else {
+      const course = await courseModel
+        .find()
+        .select(
+          "-courseData.videoUrl -courseData.suggeston -courseData.questions -courseData.links"
+        );
+      await redis.set("allCourses", JSON.stringify(course));
+
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    }
+  }
+);
+
+//GET COURSE WITH CONTENT
+export const getCourseByUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userCourseList = req.user?.courses;
+    const courseId = req.params.id;
+    const courseExistsInUser = userCourseList?.find(
+      (course: any) => course._id.toString() === courseId.toString()
+    );
+    if (!courseExistsInUser) {
+      return next(
+        new AppError("You are not eligible, please purchase to access", 401)
+      );
+    }
+    const course = await courseModel.findById(courseId);
+    const content = course?.courseData;
+    res.status(201).json({
+      success: true,
+      content,
+    });
+  }
+);
+//5:26:21
--- server/routes/course.routes.ts
+++ server/routes/course.routes.ts
@@ -1,8 +1,16 @@
 import express from "express";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import {
+  editCourse,
+  getAllCourses,
+  getCourseByUser,
+  getSingleCourse,
+  uploadCourse,
+} from "../controllers/course.controller";
 import { protect, restrictTo } from "../utils/auth";
 const router = express.Router();
-
+router.get("/getcourse/:id", getSingleCourse);
+router.get("/getcourses", getAllCourses);
+router.get("/getcoursecontent/:id", protect, getCourseByUser);
 router.post("/createcourse", protect, restrictTo("admin"), uploadCourse);
 router.patch("/editcourse", protect, restrictTo("admin"), editCourse);
 
